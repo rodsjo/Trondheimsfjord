00d5ab8dcb931f6967afb13f40319287947a4e09
Borge Rodsjo <borge.rodsjo@bouvet.no>
2015-03-26 11:13:55 +0100

    Created a Shared Xamarin.Forms project

---
+++ Trondheimsfjord/Trondheimsfjord/Trondheimsfjord.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Trondheimsfjord.WinPhone.Resources;
+
+namespace Trondheimsfjord.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
